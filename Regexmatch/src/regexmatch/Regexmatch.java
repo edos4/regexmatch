4e84674985f42ab9342016b8ba76f3d52b2d3cae
Ed Salinas <edsalinas7b@yahoo.com>
2016-09-15 08:36:21 +0800

    Reads a file

--- Regexmatch/src/regexmatch/Regexmatch.java
+++ Regexmatch/src/regexmatch/Regexmatch.java
@@ -1,5 +1,9 @@
 package regexmatch;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Regexmatch {
     
     public static boolean isTrue(String str){
@@ -10,8 +14,14 @@ public class Regexmatch {
         return str.matches(".*false.*");
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println(isTrue("True"));
         System.out.println(containsFalse("kushfdskjdgf"));
+        Scanner sc = new Scanner(new FileReader("input.txt"));
+        
+        //reads the file "input.txt"
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
     }
 }
